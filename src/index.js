22f69926e12ef89691efc0aa0a6a8c30d4d3d4c5
Roberto <robertogarcia287@gmail.com>
2023-05-15 10:13:20 -0500

    Roberto's TaskLister

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  let f = document.querySelector("form")
+  f.addEventListener('submit', (e) => {
+    e.preventDefault()
+    buildList(e.getElementById('new-task-description').value)
+    f.reset()
+  })
 });
+
+function buildList (myTodos) {
+  let p = document.createElement('p')
+  let btn = document.createElement('button')
+  btn.addEventListener('click', doDelete)
+  btn.textContent = "x"
+  p.textContent = `${myTodos}   `
+  p.appendChild(btn)
+  document.querySelector('#new-task-description').appendChild(p)
+}
+
+function doDelete(e) {
+  e.target.parentNode.remove()
+}
